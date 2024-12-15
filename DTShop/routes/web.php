fa31a97e626264b43b71efda89d14b54e46bf1b4
Nguyễn Tuấn Tú <nguyentuantu1202@gmail.com>
2024-12-15 15:42:59 +0700

    HomeController

--- DTShop/routes/web.php
+++ DTShop/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', [HomeController::class, 'index']); 
 
-Route::get('/', function () {
-    return view('layout');
-});
-Route::get('/trang-chu', function () {
-    return view('layout');
-});
+Route::get('/trang-chu', [HomeController::class, 'index']);
